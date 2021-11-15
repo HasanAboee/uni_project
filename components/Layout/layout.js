5d28e2367f130024d4fb61393ccfe8a259452b19
Hasan Aboee <h.aboee77@gmail.com>
2021-11-15 14:59:31 +0330

    icon has added to footer

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,5 +1,11 @@
 import styles from "./layout.module.css";
-
+import React from "react";
+import { ImTelegram } from "react-icons/im";
+import {
+  AiFillFacebook,
+  AiOutlineInstagram,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
 function layout(props) {
   return (
     <div className={styles.wrapper}>
@@ -9,7 +15,8 @@ function layout(props) {
         </div>
         <ul>
           <li>صفحه نخست</li>
-          <li className={styles.submenu}>چهره ها
+          <li className={styles.submenu}>
+            چهره ها
             <ul>
               <li>بازیگران</li>
               <li>آهنگسازان</li>
@@ -23,18 +30,28 @@ function layout(props) {
         <div className={styles.login_button_box}>
           <button>ورود</button>
         </div>
-        
       </nav>
       <div>{props.children}</div>
       <div className={styles.push}></div>
       <div className={styles.footer}>
-        <ul>
-          <li>بهینه سازی و توسعه توسط: حسن ابویی مهریزی</li>
-          <li>راه ارتباطی با ما</li>
-        </ul>
+      <div className={styles.contact}>راه ارتباطی با ما</div>
+        <div className={styles.icons}>
+          
+          <div className={styles.icon}>
+            <ImTelegram />
+          </div>
+          <div className={styles.icon}>
+            <AiFillFacebook />
+          </div>
+          <div className={styles.icon}>
+            <AiOutlineInstagram />
+          </div>
+          <div className={styles.icon}>
+            <AiFillTwitterCircle />
+          </div>
+        </div>
       </div>
-      </div>
-    
+    </div>
   );
 }
 export default layout;
